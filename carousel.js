62f28fa93d883edb28f389a777884816ba7b0c18
Alan Lopez <99952929+AlanLopRey@users.noreply.github.com>
2023-08-26 17:50:51 -0600

    fixing the issue with the drag and scroll

---
+++ carousel.js
@@ -0,0 +1,24 @@
+const slider = document.querySelector(".carousel");
+let isDown = false;
+let startX;
+let scrollLeft;
+
+slider.addEventListener("mousedown", (e) => {
+  isDown = true;
+  slider.classList.add("active");
+  startX = e.pageX - slider.offsetLeft;
+  scrollLeft = slider.scrollLeft;
+});
+slider.addEventListener("mouseleave", () => {
+  isDown = false;
+});
+slider.addEventListener("mouseup", () => {
+  isDown = false;
+});
+slider.addEventListener("mousemove", (e) => {
+  if (!isDown) return; //stop function
+  e.preventDefault();
+  const x = e.pageX - slider.offsetLeft;
+  const move = x - startX;
+  slider.scrollLeft = scrollLeft - move;
+});
